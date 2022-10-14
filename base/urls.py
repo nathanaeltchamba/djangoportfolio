0c36df95df1edc9cd28516fb3b03c704eec054f6
nathanaeltchamba <natanshost@gmail.com>
2022-10-14 18:01:20 -0500

    completed about section

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('profile/', views.profile, name="profile"),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
+    path('view-pdf/', views.view_pdf, name="pdf"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import FileResponse, Http404, HttpResponse
 
 # Create your views here.
 
@@ -15,3 +15,8 @@ def about(request):
 def contact(request):
     return render(request, "base/contact.html")
 
+def view_pdf(request):
+    try:
+        return FileResponse(open('resume.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
