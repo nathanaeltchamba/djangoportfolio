5c9d4736e708b59201b760c10c3beb2efaeb13ab
nathanaeltchamba <natanshost@gmail.com>
2022-10-14 21:10:22 -0500

    added more javascript

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,11 @@
+let theme = localStorage.getItem('theme')
+
+if (theme == null) {
+    setTheme('light')
+} else {
+    setTheme(theme)
+}
+
 let themeDots = document.getElementsByClassName('theme-dot')
 
 for (var i = 0; themeDots.length > i; i++) {
@@ -24,4 +32,15 @@ function setTheme(mode) {
     if (mode == 'purple') {
         document.getElementById('theme-styled').href = 'purple.css'
     }
+
+    localStorage.setItem('theme', mode)
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        })
+    })
+})
