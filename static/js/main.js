0f95e4dfac8b7345971eb65d37df38276cc54bd9
nathanaeltchamba <natanshost@gmail.com>
2022-10-14 20:02:33 -0500

    added javascript, still need to finish the theme changes

---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function () {
+        let mode = this.dataset.mode
+        console.log('Option clicked:', mode)
+        setTheme(mode)
+    })
+} 
+
+function setTheme(mode) {
+    if (mode == 'light') {
+        document.getElementById('theme-styled').href = 'main.css'
+    }
+
+    if (mode == 'blue') {
+        document.getElementById('theme-styled').href = 'blue.css'
+    }
+
+    if (mode == 'green') {
+        document.getElementById('theme-styled').href = 'green.css'
+    }
+
+    if (mode == 'purple') {
+        document.getElementById('theme-styled').href = 'purple.css'
+    }
+}
